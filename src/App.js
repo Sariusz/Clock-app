de9543e011c45f1a1395330ad242b065e8dfc78a
Sariusz <kubiczekdariusz@gmail.com>
2023-10-11 15:34:58 +0200

    randomquote fixkit and code simplification

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./scss/index.scss";
-import EveningCheck from "./js/WeatherChange.js";
+import EveningCheck from "./js/WeatherChange";
 
 const App = () => {
   return <EveningCheck />;
--- src/js/WeatherChange.js
+++ src/js/WeatherChange.js
@@ -1,126 +1,144 @@
-import React from "react";
-import { useState } from "react";
-import { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Evening from "./Evening.js";
 import Morning from "./Morning.js";
 import MorningLess from "./MorningLess.js";
 import EveningLess from "./EveningLess.js";
 
+// Function to check if it's evening based on current time
 const checkTime = () => {
-  let now = new Date();
-  let toogleComponent = new Date();
-  toogleComponent.setHours(16, 0, 0, 0);
-  if (now >= toogleComponent) {
-    return true;
-  } else {
-    return false;
-  }
+  const now = new Date();
+  const toggleComponent = new Date();
+  toggleComponent.setHours(16, 0, 0, 0);
+  return now >= toggleComponent;
 };
+
+// Function to get the first part of the author's name
+const getAuthorFirstPart = (author) => author.split(",")[0];
+
 const EveningCheck = () => {
-  const [randomQuote, setRandomQuote] = useState(true);
+  const [randomQuote, setRandomQuote] = useState({});
+  const [currentIndex, setCurrentIndex] = useState(0); // Added currentIndex
   const [pressed, setPressed] = useState(false);
   const [isEvening, setIsEvening] = useState(false);
-  const [currentTime, setCurrentTime] = useState();
-  const [currentDays, setCurrentDays] = useState();
+  const [currentTime, setCurrentTime] = useState({});
+  const [currentDays, setCurrentDays] = useState({});
+  const [quotes, setQuotes] = useState([]);
 
+  // Fetch quotes from a remote API
+  const fetchQuotes = async () => {
+    try {
+      const response = await fetch("https://type.fit/api/quotes");
+      if (!response.ok) throw new Error("Network response was not ok");
+      const json = await response.json();
+      const modifiedQuotes = json.map((quote) => ({
+        ...quote,
+        author: getAuthorFirstPart(quote.author)
+      }));
+      setQuotes(modifiedQuotes);
+      return modifiedQuotes;
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      return [];
+    }
+  };
+
+  // Function to refresh and display the next quote
   const refresh = () => {
-    fetch("https://api.api-ninjas.com/v1/quotes/", {
-      headers: { "X-Api-Key": "uyQId2ILxIJQCFf8c52XjA==dqt3pxJ8C5tcVz84" }
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        setRandomQuote(json[0]);
-      });
+    setCurrentIndex((prevIndex) => {
+      const nextIndex = (prevIndex + 1) % quotes.length;
+      const nextAuthor = getAuthorFirstPart(quotes[nextIndex].author);
+      setRandomQuote({ ...quotes[nextIndex], author: nextAuthor });
+      return nextIndex;
+    });
   };
 
-  const press = () => {
-    setPressed(!pressed);
-  };
+  // Function to handle button press
+  const press = () => setPressed(!pressed);
 
   useEffect(() => {
-    refresh();
+    // Fetch initial data
+    const fetchData = async () => {
+      const response = await fetch("https://type.fit/api/quotes");
+      if (!response.ok) throw new Error("Network response was not ok");
+      const json = await response.json();
+      const nextAuthor = getAuthorFirstPart(json[0].author);
+      setRandomQuote({ ...json[0], author: nextAuthor });
+      const quotes = await fetchQuotes();
+      setRandomQuote(quotes[0] || {});
+    };
+    fetchData();
   }, []);
+
+  useEffect(() => setIsEvening(checkTime()), []);
+
   useEffect(() => {
-    setIsEvening(checkTime());
-  }, []);
-  useEffect(() => {
+    // Fetch current day information
     fetch("http://worldtimeapi.org/api/ip")
       .then((response) => response.json())
-      .then((json) => {
-        setCurrentDays(json);
-      });
-  }, []);
-  useEffect(() => {
-    fetch("https://timezoneapi.io/api/ip/?token=aotrCOJkAzrNAMzWumKw")
-      .then((response) => response.json())
-      .then((json) => {
-        setCurrentTime(json);
-      });
+      .then((json) => setCurrentDays(json));
   }, []);
 
-  let checkEvening;
+  useEffect(() => {
+    // Fetch current time information
+    fetch("https://timezoneapi.io/api/ip/?token=aotrCOJkAzrNAMzWumKw")
+      .then((response) => response.json())
+      .then((json) => setCurrentTime(json));
+  }, []);
+
+  let checkEvening = null;
 
   if (isEvening === true) {
     if (typeof currentTime === "object" && typeof currentDays === "object") {
+      const commonProps = {
+        onPress: press,
+        onRefresh: refresh,
+        randomQuote: randomQuote.text,
+        author: randomQuote.author,
+        city: currentTime.data?.city,
+        country: currentTime.data?.timezone?.country_code,
+        time: currentTime.data?.datetime?.time,
+        timeZone: currentTime.data?.datetime?.offset_tzab
+      };
+
       checkEvening = pressed ? (
         <EveningLess
-          onPress={press}
-          onRefresh={refresh}
-          randomQuote={randomQuote.quote}
-          author={randomQuote.author}
-          city={currentTime.data.city}
-          country={currentTime.data.timezone.country_code}
-          time={currentTime.data.datetime.time}
-          timeZone={currentTime.data.datetime.offset_tzab}
-          timeZoneId={currentTime.data.timezone.id}
+          {...commonProps}
+          timeZoneId={currentTime.data?.timezone?.id}
           dayOfYear={currentDays.day_of_year}
           dayOfWeek={currentDays.day_of_week}
           weekOfYear={currentDays.week_number}
         />
       ) : (
-        <Evening
-          onPress={press}
-          onRefresh={refresh}
-          randomQuote={randomQuote.quote}
-          author={randomQuote.author}
-          city={currentTime.data.city}
-          country={currentTime.data.timezone.country_code}
-          time={currentTime.data.datetime.time}
-          timeZone={currentTime.data.datetime.offset_tzab}
-        />
+        <Evening {...commonProps} />
       );
     }
   } else {
     if (typeof currentTime === "object" && typeof currentDays === "object") {
+      const commonProps = {
+        onPress: press,
+        onRefresh: refresh,
+        randomQuote: randomQuote.text,
+        author: randomQuote.author,
+        city: currentTime.data?.city,
+        country: currentTime.data?.timezone?.country_code,
+        time: currentTime.data?.datetime?.time,
+        timeZone: currentTime.data?.datetime?.offset_tzab
+      };
+
       checkEvening = pressed ? (
         <MorningLess
-          onPress={press}
-          onRefresh={refresh}
-          randomQuote={randomQuote.quote}
-          author={randomQuote.author}
-          city={currentTime.data.city}
-          country={currentTime.data.timezone.country_code}
-          time={currentTime.data.datetime.time}
-          timeZone={currentTime.data.datetime.offset_tzab}
-          timeZoneId={currentTime.data.timezone.id}
+          {...commonProps}
+          timeZoneId={currentTime.data?.timezone?.id}
           dayOfYear={currentDays.day_of_year}
           dayOfWeek={currentDays.day_of_week}
           weekOfYear={currentDays.week_number}
         />
       ) : (
-        <Morning
-          onPress={press}
-          onRefresh={refresh}
-          randomQuote={randomQuote.quote}
-          author={randomQuote.author}
-          city={currentTime.data.city}
-          country={currentTime.data.timezone.country_code}
-          time={currentTime.data.datetime.time}
-          timeZone={currentTime.data.datetime.offset_tzab}
-        />
+        <Morning {...commonProps} />
       );
     }
   }
   return <>{checkEvening}</>;
 };
+
 export default EveningCheck;
